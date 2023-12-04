4887df4123ddaf3dd5c585152af9eb2ea512158d
Jessica Tarra <jessica@tarralva.com>
2023-12-04 20:59:39 -0300

    Add validation tests to app impl

--- ms/auth/app/app_test.go
+++ ms/auth/app/app_test.go
@@ -1,11 +1,48 @@
 package app
 
 import (
+	"github.com/jessicatarra/greenlight/internal/validator"
+	"github.com/jessicatarra/greenlight/ms/auth/entity"
 	"github.com/jessicatarra/greenlight/ms/auth/repository/mocks"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"testing"
 )
+
+func TestValidateUser(t *testing.T) {
+	v := validator.New()
+
+	password := "password123!"
+	hash := []byte("sampleHash")
+
+	user := &entity.User{
+		Name:     "John Doe",
+		Email:    "john@example.com",
+		Password: entity.Password{Plaintext: &password, Hash: hash},
+	}
+
+	ValidateUser(v, user)
+
+	assert.True(t, v.Valid())
+}
+
+func TestValidateEmail(t *testing.T) {
+	v := validator.New()
+	email := "john@example.com"
+
+	ValidateEmail(v, email)
+
+	assert.True(t, v.Valid())
+}
+
+func TestValidatePasswordPlaintext(t *testing.T) {
+	v := validator.New()
+	password := "password123"
+
+	ValidatePasswordPlaintext(v, password)
+
+	assert.True(t, v.Valid())
+}
 
 func TestCreate(t *testing.T) {
 	// Initialize the repository mock
