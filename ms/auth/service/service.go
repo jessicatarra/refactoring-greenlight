c616f9b823112dba1216dc08fb1bf458a82e127d
Jessica Tarra <jessica@tarralva.com>
2023-12-06 20:23:04 -0300

    Update auth service
    By extract user validation into separate file. Also update mocks.

--- ms/auth/service/service.go
+++ ms/auth/service/service.go
@@ -2,10 +2,8 @@ package service
 
 import (
 	"github.com/jessicatarra/greenlight/internal/errors"
-	"github.com/jessicatarra/greenlight/internal/password"
 	"github.com/jessicatarra/greenlight/internal/request"
 	"github.com/jessicatarra/greenlight/internal/response"
-	"github.com/jessicatarra/greenlight/internal/utils"
 	"github.com/jessicatarra/greenlight/ms/auth/domain"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
@@ -62,28 +60,6 @@ func (r *resource) create(res http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		errors.ServerError(res, req, err)
 	}
-}
-
-func ValidateUser(input domain.CreateUserRequest, existingUser *domain.User) {
-	input.Validator.CheckField(input.Name != "", "name", "must be provided")
-	input.Validator.CheckField(len(input.Name) <= 500, "name", "must not be more than 500 bytes long")
-
-	ValidateEmail(input, existingUser)
-
-	ValidatePassword(input)
-}
-
-func ValidatePassword(input domain.CreateUserRequest) {
-	input.Validator.CheckField(input.Password != "", "Password", "Password is required")
-	input.Validator.CheckField(len(input.Password) >= 8, "Password", "Password is too short")
-	input.Validator.CheckField(len(input.Password) <= 72, "Password", "Password is too long")
-	input.Validator.CheckField(utils.NotIn(input.Password, password.CommonPasswords...), "Password", "Password is too common")
-}
-
-func ValidateEmail(input domain.CreateUserRequest, existingUser *domain.User) {
-	input.Validator.CheckField(input.Email != "", "Email", "Email is required")
-	input.Validator.CheckField(utils.Matches(input.Email, utils.RgxEmail), "Email", "Must be a valid email address")
-	input.Validator.CheckField(existingUser == nil, "Email", "Email is already in use")
 }
 
 func (r *resource) activate(res http.ResponseWriter, req *http.Request) {
---
+++ ms/auth/service/validation.go
@@ -0,0 +1,29 @@
+package service
+
+import (
+	"github.com/jessicatarra/greenlight/internal/password"
+	"github.com/jessicatarra/greenlight/internal/utils"
+	"github.com/jessicatarra/greenlight/ms/auth/domain"
+)
+
+func ValidateUser(input domain.CreateUserRequest, existingUser *domain.User) {
+	input.Validator.CheckField(input.Name != "", "name", "must be provided")
+	input.Validator.CheckField(len(input.Name) <= 500, "name", "must not be more than 500 bytes long")
+
+	ValidateEmail(input, existingUser)
+
+	ValidatePassword(input)
+}
+
+func ValidatePassword(input domain.CreateUserRequest) {
+	input.Validator.CheckField(input.Password != "", "Password", "Password is required")
+	input.Validator.CheckField(len(input.Password) >= 8, "Password", "Password is too short")
+	input.Validator.CheckField(len(input.Password) <= 72, "Password", "Password is too long")
+	input.Validator.CheckField(utils.NotIn(input.Password, password.CommonPasswords...), "Password", "Password is too common")
+}
+
+func ValidateEmail(input domain.CreateUserRequest, existingUser *domain.User) {
+	input.Validator.CheckField(input.Email != "", "Email", "Email is required")
+	input.Validator.CheckField(utils.Matches(input.Email, utils.RgxEmail), "Email", "Must be a valid email address")
+	input.Validator.CheckField(existingUser == nil, "Email", "Email is already in use")
+}
